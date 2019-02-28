d9a5bf0faf2c9b303c86570549c42e06df02cf15
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2019-02-28 17:15:06 +0200

    Activated service worker for offline status

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,10 +1,8 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Query } from 'react-apollo'
-import { Offline, Online } from 'react-detect-offline'
 
 import NavBar from '../../components/NavBar'
 import SideBar from '../../components/SideBar'
-import OfflinePage from '../OfflinePage'
 
 import { withAuth } from '../../hocs/PrivateRoute'
 import { User } from '../../queries'
@@ -12,24 +10,17 @@ import { User } from '../../queries'
 import '../../index.css'
 
 const Main = props => (
-  <Fragment>
-    <Offline>
-      <OfflinePage />
-    </Offline>
-    <Online>
-      <div className="flexible-content">
-        <Query query={User}>
-          {({ loading, error, data }) => {
-            if (loading) return 'Loading...'
-            if (error) return `Error! ${error.message}`
-            return <SideBar {...data.me} />
-          }}
-        </Query>
-        <NavBar />
-        {props.children}
-      </div>
-    </Online>
-  </Fragment>
+  <div className="flexible-content">
+    <Query query={User}>
+      {({ loading, error, data }) => {
+        if (loading) return 'Loading...'
+        if (error) return `Error! ${error.message}`
+        return <SideBar {...data.me} />
+      }}
+    </Query>
+    <NavBar />
+    {props.children}
+  </div>
 )
 
 export default withAuth(Main)
--- client/src/pages/Room/index.js
+++ client/src/pages/Room/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Query, Subscription } from 'react-apollo'
 import { MDBContainer } from 'mdbreact'
+import { Offline, Online } from 'react-detect-offline'
 
 import {
   getRoom,
@@ -10,6 +11,7 @@ import {
 
 import UserInfo from '../../components/UserProfile'
 import CreateMessageForm from '../../components/Forms/CreateMessageForm'
+import OfflinePage from '../../components/OfflinePage'
 import MessageList from './MessageList'
 
 const Room = props => {
@@ -46,32 +48,37 @@ const Room = props => {
         return (
           <MDBContainer>
             <CreateMessageForm currentRoom={currentRoom} {...data.room} />
-            <MessageList
-              data={data.room}
-              currentRoom={currentRoom}
-              onLoadMore={() =>
-                fetchMore({
-                  variables: {
-                    id: currentRoom,
-                    first: 5,
-                    skip: data.room.messages.length
-                  },
-                  updateQuery: (prev, { fetchMoreResult }) => {
-                    if (!fetchMoreResult) return prev
-                    return Object.assign({}, prev, {
-                      room: {
-                        messages: [
-                          ...prev.room.messages,
-                          ...fetchMoreResult.room.messages
-                        ],
-                        users: prev.room.users,
-                        __typename: prev.room.__typename
-                      }
-                    })
-                  }
-                })
-              }
-            />
+            <Offline>
+              <OfflinePage />
+            </Offline>
+            <Online>
+              <MessageList
+                data={data.room}
+                currentRoom={currentRoom}
+                onLoadMore={() =>
+                  fetchMore({
+                    variables: {
+                      id: currentRoom,
+                      first: 5,
+                      skip: data.room.messages.length
+                    },
+                    updateQuery: (prev, { fetchMoreResult }) => {
+                      if (!fetchMoreResult) return prev
+                      return Object.assign({}, prev, {
+                        room: {
+                          messages: [
+                            ...prev.room.messages,
+                            ...fetchMoreResult.room.messages
+                          ],
+                          users: prev.room.users,
+                          __typename: prev.room.__typename
+                        }
+                      })
+                    }
+                  })
+                }
+              />
+            </Online>
             <div className="bar-right position-fixed shade">
               <UserInfo profile={data.room.users[0]} />
             </div>
